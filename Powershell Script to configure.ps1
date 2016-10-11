8683f750dbcee19decc914d5d0ac427f86982289
Patrick Alexander <PatrickAlexander@live.com>
2016-10-10 20:20:51 -0700

    Key Vault Demo

---
+++ Powershell Script to configure.ps1
@@ -0,0 +1,17 @@
+﻿
+Login-AzureRmAccount
+
+$resources = Get-AzureRmResourceProvider -ProviderNamespace Microsoft.Compute
+$resources.ResourceTypes.Where{($_.ResourceTypeName -eq 'virtualMachines')}.Locations
+
+#create RG
+New-AzureRmResourceGroup -Name KeyVaultRG -Location "West US"
+
+#Create keyvault
+New-AzureRmKeyVault -VaultName "mykeyvaultfortesting" -ResourceGroupName "KeyVaultRG" -Location "West US"
+
+#Access to user
+Set-AzureRmKeyVaultAccessPolicy -VaultName "mykeyvaultfortesting" -ResourceGroupName "KeyVaultRG" -PermissionsToKeys create,get,wrapKey,unwrapKey,sign,verify  -UserPrincipalName "paalexan@microsoft.com"
+
+#Azure Active Directory Application Client ID
+Set-AzureRmKeyVaultAccessPolicy -VaultName "mykeyvaultfortesting" -ResourceGroupName "KeyVaultRG" -ServicePrincipalName "b0b2a0df-b409-43cf-906f-6723ce471527" -PermissionsToKeys get,wrapKey,unwrapKey,sign,verify
